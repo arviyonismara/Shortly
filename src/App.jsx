5bcd47378b5073fe3f122eaa49f70ec55e7d693f
Arvie Yonismara <arviyonismara@gmail.com>
2025-03-23 23:09:06 +0700

    add Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,36 @@ function App() {
 
         {/* Todo Mobile Menu */}
       </nav>
+
+      {/* Hero Section */}
+      <section id="hero">
+        {/* Hero Container */}
+        <div className="container flex flex-col-reverse mx-auto p-6 lg:flex-row">
+          {/* Content Container */}
+          <div className="flex flex-col space-y-10 mb-44 lg:mt-16 lg:w-1/2 xl:mb-52">
+            <h1 className="text-5xl font-bold text-center text-veryDarkViolet lg:text-left lg:text-6xl">
+              {" "}
+              More Than just Shorter Links
+            </h1>
+            <p className="text-2xl text-center text-gray-400 lg:max-w-md lg:text-left">
+              Build your brand's recognition and get detailed insights on how
+              your links are performing.
+            </p>
+            <div className="mx-auto lg:mx-0">
+              <a
+                href=""
+                className="py-5 px-10 text-2xl font-bold text-white bg-cyan rounded-full lg:py-4 hover:opacity-70"
+              >
+                Get Started
+              </a>
+            </div>
+          </div>
+          {/* Image */}
+          <div className="mb-24 mx-auto md:w-180 lg:w-1/2 lg:mb-0">
+            <img src="images/illustration-working.svg" alt="" />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
