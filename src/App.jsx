ee68742f0fa6bdba037d297b66a31954072d0a26
Arvie Yonismara <arviyonismara@gmail.com>
2025-03-25 02:26:18 +0700

    add CTA and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -88,9 +88,15 @@ function App() {
             <button className="px-10 py-3 text-white bg-cyan rounded-lg hover:bg-cyanLight focus:outline-none md:py-2">
               Shorten It!
             </button>
+
+            {/* Error Message */}
+            <div
+              id="err-msg"
+              className="absolute left-7 bottom-3 text-red text-sm italic"
+            ></div>
           </form>
 
-          {/* Link 2 */}
+          {/* Link 1 */}
           <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
             <p className="font-bold text-center text-veryDarkViolet md:text-left">
               https://www.frontendmentor.io
@@ -139,6 +145,210 @@ function App() {
           </div>
         </div>
       </section>
+
+      {/* Stats Section */}
+      <section id="stats" className="py-24 bg-gray-100">
+        <div className="container mx-auto px-3">
+          <h2 className="text-4xl font-bold text-center mb-6">
+            Advance Statistics
+          </h2>
+          <p className="max-w-xs mx-auto text-center text-gray-400 md:max-w-md">
+            Track how your links are performing across the web with our advanced
+            statistics dashboard
+          </p>
+        </div>
+      </section>
+
+      {/* Feature Box Section */}
+      <section id="features" className="pb-32 bg-gray-100">
+        <div className="relative container flex flex-col items-start px-6 mx-auto md:flex-row md:space-x-7">
+          {/* Horizontal Line */}
+          <div className="hidden absolute top-24 w-10/12 left-16 h-3 bg-cyan md:block"></div>
+          {/* Vertical Line */}
+          <div className="absolute w-2 h-full -ml-1 left-1/2 bg-cyan md:hidden"></div>
+
+          {/* Box 1 */}
+          <div className="relative flex flex-col p-6 space-y-6 bg-white rounded-lg md:w-1/3">
+            {/* Image Positioning */}
+            <div className="absolute -ml-10 left-1/2 -top-10 md:left-16">
+              {/* Image Container For Background & Center */}
+              <div className="flex items-center justify-center w-20 h-20 p-4 rounded-full bg-veryDarkViolet">
+                <img src="images/icon-brand-recognition.svg" alt="" />
+              </div>
+            </div>
+            <h5 className="pt-6 text-xl font-bold text-center capitalize md:text-left">
+              Brand Recognition
+            </h5>
+            <p className="text-center text-gray-400 md:text-left">
+              Boost your brand recognition with each click. Generic links don't
+              mean a thing. Branded links help instil confidence in your
+              content.
+            </p>
+          </div>
+
+          {/* Box 2 */}
+          <div className="relative flex flex-col mt-24 p-6 space-y-6 bg-white rounded-lg md:w-1/3 md:mt-8">
+            {/* Image Positioning */}
+            <div className="absolute -ml-10 left-1/2 -top-10 md:left-16">
+              {/* Image Container For Background & Center */}
+              <div className="flex items-center justify-center w-20 h-20 p-4 rounded-full bg-veryDarkViolet">
+                <img src="images/icon-detailed-records.svg" alt="" />
+              </div>
+            </div>
+            <h5 className="pt-6 text-xl font-bold text-center capitalize md:text-left">
+              Detail Records
+            </h5>
+            <p className="text-center text-gray-400 md:text-left">
+              Boost your brand recognition with each click. Generic links don't
+              mean a thing. Branded links help instil confidence in your
+              content.
+            </p>
+          </div>
+
+          {/* Box 3 */}
+          <div className="relative flex flex-col mt-24 p-6 space-y-6 bg-white rounded-lg md:w-1/3 md:mt-8">
+            {/* Image Positioning */}
+            <div className="absolute -ml-10 left-1/2 -top-10 md:left-16">
+              {/* Image Container For Background & Center */}
+              <div className="flex items-center justify-center w-20 h-20 p-4 rounded-full bg-veryDarkViolet">
+                <img src="images/icon-fully-customizable.svg" alt="" />
+              </div>
+            </div>
+            <h5 className="pt-6 text-xl font-bold text-center capitalize md:text-left">
+              Fully Customizable
+            </h5>
+            <p className="text-center text-gray-400 md:text-left">
+              Boost your brand recognition with each click. Generic links don't
+              mean a thing. Branded links help instil confidence in your
+              content.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      {/* CTA Section */}
+      <section id="cta" className="py-24 bg-darkViolet">
+        <div className="flex flex-col p-2 space-y-6">
+          <h5 className="mx-auto space-y-10 text-4xl font-bold text-center text-white">
+            Boost your links today
+          </h5>
+          <button className="px-10 py-5 mx-auto text-2xl font-bold text-white rounded-full bg-cyan hover:bg-cyanLight md:text-base md:py-3 focus:outline-none">
+            Get Started
+          </button>
+        </div>
+      </section>
+
+      <footer className="py-16 bg-veryDarkViolet">
+        <div className="container flex flex-col items-center justify-between mx-auto space-y-16 md:flex-row md:space-y-0 md:items-start">
+          {/* Logo */}
+          <img src="images/logo.svg" alt="" />
+          {/* Menus Container */}
+          <div className="flex flex-col space-y-16 md:space-x-20 md:flex-row md:space-y-0">
+            {/* Menu 1 */}
+            <div className="flex flex-col items-center w-full md:items-start">
+              <div className="mb-5 font-bold text-white capitalize">
+                Features
+              </div>
+              <div className="flex flex-col items-center space-y-3 md:items-start">
+                <a
+                  href=""
+                  className="capitalize text-grayishViolet hover:text-cyan"
+                >
+                  Link Shortening
+                </a>
+                <a
+                  href=""
+                  className="capitalize text-grayishViolet hover:text-cyan"
+                >
+                  Link Shortening
+                </a>
+                <a
+                  href=""
+                  className="capitalize text-grayishViolet hover:text-cyan"
+                >
+                  Link Shortening
+                </a>
+              </div>
+            </div>
+
+            {/* Menu 2 */}
+            <div className="flex flex-col items-center w-full md:items-start">
+              <div className="mb-5 font-bold text-white capitalize">
+                Resources
+              </div>
+              <div className="flex flex-col items-center space-y-3 md:items-start">
+                <a
+                  href=""
+                  className="capitalize text-grayishViolet hover:text-cyan"
+                >
+                  Blog
+                </a>
+                <a
+                  href=""
+                  className="capitalize text-grayishViolet hover:text-cyan"
+                >
+                  Developers
+                </a>
+                <a
+                  href=""
+                  className="capitalize text-grayishViolet hover:text-cyan"
+                >
+                  Support
+                </a>
+              </div>
+            </div>
+
+            {/* Menu 3 */}
+            <div className="flex flex-col items-center w-full md:items-start">
+              <div className="mb-5 font-bold text-white capitalize">
+                Company
+              </div>
+              <div className="flex flex-col items-center space-y-3 md:items-start">
+                <a
+                  href=""
+                  className="capitalize text-grayishViolet hover:text-cyan"
+                >
+                  About
+                </a>
+                <a
+                  href=""
+                  className="capitalize text-grayishViolet hover:text-cyan"
+                >
+                  Our Team
+                </a>
+                <a
+                  href=""
+                  className="capitalize text-grayishViolet hover:text-cyan"
+                >
+                  Careers
+                </a>
+                <a
+                  href=""
+                  className="capitalize text-grayishViolet hover:text-cyan"
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Social Container*/}
+          <div className="flex space-x-6">
+            <a href="">
+              <img src="images/icon-facebook.svg" alt="" className="ficon" />
+            </a>
+            <a href="">
+              <img src="images/icon-twitter.svg" alt="" className="ficon" />
+            </a>
+            <a href="">
+              <img src="images/icon-instagram.svg" alt="" className="ficon" />
+            </a>
+            <a href="">
+              <img src="images/icon-pinterest.svg" alt="" className="ficon" />
+            </a>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
